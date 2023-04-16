8e7102be9822c71a2093bd2655af035a3df1e6b5
fb0801 <fbhatti0801@gmail.com>
2023-04-16 14:37:12 +0100

    js writing

--- spaceInvaders/app.js
+++ spaceInvaders/app.js
@@ -1,3 +1,25 @@
-document.addEventListener('DOMContentLoaded', () = {
-    const square 
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
+    
+    let width = 15
+    let currentShooterIndex = 202
+    let currentInvaderIndex = 0
+    let alienInvadersTakenDown = []
+    let result = 0
+    let direction =1
+    let invaderID 
+
+    //def alien
+    const alienInvaders = [
+        0,1,2,3,4,5,6,7,8,9,
+        15,16,17,18,19,20,21,22,23,24,
+        30,31,32,33,34,35,36,37,38,39
+    ]
+    //draw aliens
+    alienInvaders.forEach( invader => squares[currentInvaderIndex + invader].classList.add('invader'))
+
+
+    //draw shooter
+    squares[currentShooterIndex].classList.add('shooter')
 })
